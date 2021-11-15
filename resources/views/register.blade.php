cda208e60260203b919fbc0a867f64c01272dca7
Thania02 <74527397+Thania02@users.noreply.github.com>
2021-11-16 02:02:11 +0800

    REGISTER PAGE

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,29 +1,52 @@
-<!doctype html>
-<html lang="en">
-<head>
-    @include('includes.head')
-    <title>Register</title>
-    <!-- Custom styles for this template -->
-    
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Registration</title>
+        <link rel="stylesheet" href="css/register.css"/>
 </head>
-
 <body>
+    <?php
 
-    <header class="row">
-        @include('includes.header')
-    </header>
-
+    if (isset($_REQUEST['username'])){
+        $username = stripcslashes($_REQUEST['username']);
+        $username = mysqli_real_escape_string($con,$username);
+        $email = stripcslashes($_REQUEST['email']);
+        $email = mysqli_real_escape_string($con,$email);
+        $password = stripcslashes($_REQUEST['password']);
+        $password = mysqli_real_escape-string($con,$password);
+        $birthday = stripcslashes($_REQUEST['birthday']);
+        $birthday = mysqli_real_escape_string($con,$birthday);
+        $address = stripcslashes($_REQUEST['address']);
+        $address = mysqli_real_escape_string($con,$address);
+        $query = "INSET into 'user' (username, password, email, birthday, address)
+        VALUES ('$username', '".md5($password)."', '$email', '$birthday', '$address')";
+        $result = mysqli_query($con,$query);
+        if($result){
+            echo "<div class='form'>
+        <h3>You are registered successfully</h3>
+        <br/>Click here to <a href='login.php'>Login</a></div>";
+    }
+}else{
+    ?>
     <div class="col-lg-8 mx-auto p-3 py-md-5">
-        
-        <main>
-            <h1>Register Your Account</h1>
-            
-        </main>
-
-        <footer class="pt-5 my-5 text-muted border-top">
-            Created by the CareWare team &middot; &copy; 2021
-        </footer>
+    <div class="form">
+        <h1> Registration </h1>
+        <form name="registration" action="" method="post">
+        <input type="text" name="username" placeholder="Username" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <input type="text" name="birthday" placeholder="Birthday" required />
+        <input type="text" name="address" placeholder="Address" required /> <br>
+        <label for="cars">Status:</label>
+  <select name="status" id="status">
+    <option value="senior">Senior Citizen</option>
+    <option value="care">Care Tarker</option>
+  </select><br>
+        <input type="submit" name="submit" value="Register" />
+    </form>
     </div>
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+</div>
+    <?php  }?>
 </body>
 </html>
