6fbf37d7c98552befdf518d8ddcd35c6fee4b881
Muhammad Rofiqurrahman <mhdrfq.rahman@gmail.com>
2021-11-15 10:25:34 +0800

    update header and navigation

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,22 +10,25 @@
     
 <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">CareWear</a>
+    <a class="navbar-brand" href="{{url('home')}}">CareWear</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item">
-          <!--put this is in class to highlight current page: active" aria-current="page"-->
-          <a class="nav-link" href="{{url('home')}}">Home</a>
-        </li>
+        <!--put this is in class to highlight current page: active" aria-current="page"-->
         <li class="nav-item">
           <a class="nav-link" href="{{url('profile')}}">Profile</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="{{url('evaluate')}}">Evaluate Condition</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{url('record')}}">Data Record</a>
+        </li>
+        <!--<li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>-->
       </ul>
     </div>
   </div>
