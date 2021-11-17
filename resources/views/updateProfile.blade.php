2a078a2a02f47b39bd541302a70b297a30737168
daarienz <67551432+daarienz@users.noreply.github.com>
2021-11-18 03:04:29 +0700

    Modify updateProfile.css, updateprofile.blade.php (2)

--- resources/views/updateProfile.blade.php
+++ resources/views/updateProfile.blade.php
@@ -15,27 +15,23 @@
     </header>
     <section class="section about-section gray-bg" id="about">
         <div class="col-lg-8 mx-auto p-3 py-md-5">
-
             <div class="container emp-profile">
-
                 <div class="row">
                     <!-- left column -->
                     <div class="col-md-4">
-                        <h2>Edit Profile</h2>
-                        <div class="text-center">
-                            <img src="https://bootdey.com/img/Content/avatar/avatar7.png" class="avatar img-circle img-thumbnail" alt="avatar" style="border-radius: 50%;"> </br> </br>
+                        <div class="profile-img">
+                            <h2>Edit Profile</h2> <br>
+                            <img src="https://i.ibb.co/XDvNnq4/IU-Photos-107-of-599-Last-fm.webp" alt="" style="border-radius: 50%;"> </br> </br> </br>
                             <h5>Senior Citizen</h5>
                         </div>
                     </div>
-
                     <!-- edit form column -->
-                    <div class="col-md-6 personal-info">
-                        <h3>Personal info</h3>
-
+                    <div class="col-md-6">
+                        <h4>Personal info</h4> </br>
                         <form class="form-horizontal" role="form">
                             <div class="form-group">
                                 <label class="col-lg-3 control-label">Full Name:</label>
-                                <div class="col-lg-8">
+                                <div class="col-lg-8"> 
                                     <input class="form-control" type="text" value="">
                                 </div>
                             </div>
@@ -73,14 +69,14 @@
                                 <label class="col-lg-3 control-label">Address:</label>
                                 <div class="col-lg-8">
                                     <input class="form-control" type="text" value="">
-                                </div>
+                                </div> </br>
                             </div>
                         </form>
                     </div>
 
                     <!-- Submit form column -->
                     <div class="col-md-2">
-                        <input type="submit">
+                        <input type="submit" class="profile-edit-btn">
                     </div>
                 </div>
             </div>
