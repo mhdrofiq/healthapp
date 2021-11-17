3a1a2a4caaee5c90367dc87e53323bc82dda25d9
Thania02 <74527397+Thania02@users.noreply.github.com>
2021-11-17 23:24:55 +0800

    Register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,52 +1,127 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Registration</title>
-        <link rel="stylesheet" href="css/register.css"/>
+<!doctype html>
+<html lang="en">
+<head>
+    @include('includes.head')
+    <title>Register</title>
+    <link rel="stylesheet" href="css/register.css"/>
+    <!-- Custom styles for this template -->
+    
 </head>
 <body>
-    <?php
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row justify-content-center align-items-center h-100">
+      <div class="col-12 col-lg-9 col-xl-7">
+        <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+          <div class="card-body p-4 p-md-5">
+            <center>  
+            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form </h3> </center>
+            <form>
 
-    if (isset($_REQUEST['username'])){
-        $username = stripcslashes($_REQUEST['username']);
-        $username = mysqli_real_escape_string($con,$username);
-        $email = stripcslashes($_REQUEST['email']);
-        $email = mysqli_real_escape_string($con,$email);
-        $password = stripcslashes($_REQUEST['password']);
-        $password = mysqli_real_escape-string($con,$password);
-        $birthday = stripcslashes($_REQUEST['birthday']);
-        $birthday = mysqli_real_escape_string($con,$birthday);
-        $address = stripcslashes($_REQUEST['address']);
-        $address = mysqli_real_escape_string($con,$address);
-        $query = "INSET into 'user' (username, password, email, birthday, address)
-        VALUES ('$username', '".md5($password)."', '$email', '$birthday', '$address')";
-        $result = mysqli_query($con,$query);
-        if($result){
-            echo "<div class='form'>
-        <h3>You are registered successfully</h3>
-        <br/>Click here to <a href='login.php'>Login</a></div>";
-    }
-}else{
-    ?>
-    <div class="col-lg-8 mx-auto p-3 py-md-5">
-    <div class="form">
-        <h1> Registration </h1>
-        <form name="registration" action="" method="post">
-        <input type="text" name="username" placeholder="Username" required />
-        <input type="email" name="email" placeholder="Email" required />
-        <input type="password" name="password" placeholder="Password" required />
-        <input type="text" name="birthday" placeholder="Birthday" required />
-        <input type="text" name="address" placeholder="Address" required /> <br>
-        <label for="cars">Status:</label>
-  <select name="status" id="status">
-    <option value="senior">Senior Citizen</option>
-    <option value="care">Care Tarker</option>
-  </select><br>
-        <input type="submit" name="submit" value="Register" />
-    </form>
+              <div class="row">
+                <div class="col-md-6 mb-4">
+
+                  <div class="form-outline">
+                  <input type="name" class="form-control form-control-lg" placeholder="Name" />
+                    
+                  </div>
+
+                </div>
+                <div class="col-md-6 mb-4">
+
+                  <div class="form-outline">
+                    <input type="text" id="email" class="form-control form-control-lg" placeholder="Email" />
+                    
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-4 d-flex align-items-center">
+
+                  <div class="form-outline w-100">
+                    <input
+                      type="text"
+                      class="form-control form-control-lg"
+                      id="birthdayDate"
+                      placeholder="Birthday Date"
+                      onfocus="(this.type='date')"
+                    />
+                    
+                  </div>
+
+                </div>
+                <div class="col-md-6 mb-4">
+
+                  <h6 class="mb-2 pb-1">Gender: </h6>
+
+                  <div class="form-check form-check-inline">
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      name="inlineRadioOptions"
+                      id="femaleGender"
+                      value="option1"
+                      checked
+                    />
+                    <label class="form-check-label" for="femaleGender">Female</label>
+                  </div>
+
+                  <div class="form-check form-check-inline">
+                    <input
+                      class="form-check-input"
+                      type="radio"
+                      name="inlineRadioOptions"
+                      id="maleGender"
+                      value="option2"
+                    />
+                    <label class="form-check-label" for="maleGender">Male</label>
+                   
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-4 d-flex align-items-center">
+
+                  <div class="form-outline w-100 ">
+                     <input type="text" id="Address" class="form-control form-control-lg" placeholder="Address" />
+                  </div>
+
+                </div>
+                <div class="col-md-6 mb-4">
+
+                  <div class="form-outline">
+                     <input type="text" id="PhoneNum" class="form-control form-control-lg" placeholder="Phone Number" />
+                   
+                  </div>
+
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-12">
+
+                  <select class="select form-control-lg">
+                    
+                    <option value="2">Senior Citizen</option>
+                    <option value="3">Care Taker</option>
+                    
+                  </select>
+                  <label class="form-label select-label" >Register As</label>
+
+                </div>
+              </div>
+            <center>
+              <div class="mt-4 pt-2">
+                <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
+              <input class="btn btn-primary btn-lg" type="button" name="cancel" value="cancel"  />       
+            </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-    <?php  }?>
-</body>
-</html>
+  </div>
+</section> 
