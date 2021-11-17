7eea07991b70f0cb02b735dd00fb24073bb5250f
IjakHobi <89329438+IjakHobi@users.noreply.github.com>
2021-11-17 22:41:20 +0800

    help problem

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     @include('includes.head')
     <title>Profile</title>
@@ -13,61 +14,82 @@
         @include('includes.header')
     </header>
 
-        
-        <div class="col-lg-8 mx-auto p-3 py-md-5">
-        <h2 class="light-color">My Profile</h2>
-        <section class="section about-section gray-bg" id="about">
-            <div class="container">
-                <div class="row align-items-center flex-row-reverse">
-                    <div class="col-lg-6">
-                        <div class="about-text go-to">
-                            <h3 class="dark-color">Muhammad Junayd</h3>
-                            <div class="row about-list">
-                                <div class="col-md-6">
-                                    <div class="media">
-                                        <label>Birthday</label>
-                                        <p>4th april 1998</p>
-                                    </div>
-                                    <div class="media">
-                                        <label>Age</label>
-                                        <p>22 Year</p>
-                                    </div>
-                                    
-                                    <div class="media">
-                                        <label>Address</label>
-                                        <p>Padang, West Sumatra</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="media">
-                                        <label>E-mail</label>
-                                        <p>mhdjunayd@gmail.com</p>
-                                    </div>
-                                    <div class="media">
-                                        <label>Phone</label>
-                                        <p>081245896378</p>
-                                    </div>
-                                </div>
-                            </div>
+
+    <div class="container emp-profile">
+        <form method="post">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="profile-img">
+                        <img style="border-radius: 50%;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
+                        <br><br><br>
+                        <div class="file btn btn-lg btn-primary">
+                            Change Photo
+                            <input type="file" name="file" />
                         </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="about-avatar">
-                            <img class="picture" src="https://i.ibb.co/YRFpKHx/16-168770-user-iconset-no-profile-picture-icon-circle-clipart-removebg-preview.png" title="" alt="">
-                            
-                        </div>
-                        <h4 style="margin-left: 35%;">Senior Citizen</h4>
-                        <button class="button" type="button" onclick="color()">Edit Profile</button>
                     </div>
                 </div>
-            </div>
-        </section>
-        </div>
+                <div class="col-md-6">
+                    <div class="profile-head">
+                        <h5>
+                            Kshiti Ghelani
+                        </h5>
+                        <h6>
+                            Web Developer and Designer
+                        </h6>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>User Id</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Kshiti123</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Name</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Kshiti Ghelani</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Email</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>kshitighelani@gmail.com</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Phone</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>123 456 7890</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label>Profession</label>
+                            </div>
+                            <div class="col-md-6">
+                                <p>Web Developer and Designer</p>
+                            </div>
+                        </div>
 
-        <footer class="pt-5 my-5 text-muted border-top">
-            Created by the CareWare team &middot; &copy; 2021
-        </footer>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile" />
+                </div>
+            </div>
+
+        </form>
     </div>
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+    
+    <script src="../assets/dist/js/bootstrap.bundle.min.js">
+    </script>
 </body>
+
 </html>
