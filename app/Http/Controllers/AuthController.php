4f94f71272333a6f7014ccea2582a80be2d8da8b
Muhammad Rofiqurrahman <mhdrfq.rahman@gmail.com>
2021-11-21 13:22:33 +0800

    register progress

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("home")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'phone' => $data['phone'],
+        'gender' => $data['gender'],
+        'address' => $data['address'],
+        'birthdate' => $data['birthdate'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }    
+}
