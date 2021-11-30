365c9f7aab98f7cb95f4f202377cdb3f47c98779
daarienz <67551432+daarienz@users.noreply.github.com>
2021-11-30 22:41:27 +0700

    Add changePassword.blade.php, modify UserController.php, modify updateProfile.css, modify web.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use App\Models\User;
 
 class UserController extends Controller
@@ -14,20 +17,65 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\View\View
      */
-    
-    public function updateProfile()
+
+    public function updateProfileView()
     {
-        return view('updateProfile');
-        $User = Auth::user();
-        $profile = User::where($User)->get();
+        $data = User::all();
+        return view('updateProfile', ['data' => $data]);
+    }
 
+    public function updateProfile(Request $request)
+    {
+        $user = User::find($request->id);
 
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->gender = $request->input('gender');
+        $user->birthdate = $request->birthdate;
+        $user->phone = $request->phone;
+        $user->address = $request->address;
+        $user->save();
+
+        echo "<script>";
+        echo "alert('Profile updated successfully !');";
+        echo "</script>";
         
+        return view('profile');
+    }
+
+    public function changePasswordView()
+    {
+        return view('changePassword');
     }
 
 
-    public function changePassword(){
-        
+    public function changePassword(Request $request)
+    {
+        $user = User::find($request->id);
+
+        if (Hash::check($request->oldPassword, $user->password)) {
+            if ($request->newPassword == $request->confirmNewPassword) {
+                $user->password = $request->newPassword;
+                $user->save();
+
+                echo "<script>";
+                echo "alert('Password changed successfully !');";
+                echo "</script>";
+
+                return view('profile');
+            } else {
+                echo "<script>";
+                echo "alert('Confirm new password must be the same as new password !');";
+                echo "</script>";
+
+                return view('changePassword');
+            }
+        } else {
+            echo "<script>";
+            echo "alert('Wrong Password !');";
+            echo "</script>";
+
+            return view('changePassword');
+        }
     }
-    
 }
